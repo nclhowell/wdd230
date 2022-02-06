8d3a958675ded76928149f31726886d6177a7520
Layne <nclhowell@gmail.com>
2022-02-05 20:11:12 -0800

    imagaload 5

--- imageload/js/app.js
+++ imageload/js/app.js
@@ -6,11 +6,6 @@ const loadImages = (image) => {
   };
 };
 
-setTimeout(() => {imagesToLoad.forEach((img) => {
+setTimeout(() => { imagesToLoad.forEach((img) => {
   loadImages(img);
-});
-<<<<<<< HEAD
-}, 7000);
-=======
-}, 5000);
->>>>>>> a8a69c7c8d64053f3e4bfb6775bdf0e81f0aeb0a
+})}, 5000)
