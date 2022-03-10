10f37cc9e543cb1a7357472dfe67b6193baa6960
Layne <nclhowell@gmail.com>
2022-03-09 17:32:53 -0800

    weather

---
+++ lesson10/js/todaydate.js
@@ -0,0 +1,42 @@
+// call date function and store output in date_today variable 
+let date_today = new Date();
+
+// initalize and populate variable to hold the current month
+let month_today = date_today.getMonth() + 1
+
+// initalize and populate variable to hold the current day
+let day_today = date_today.getDay()
+
+// initialize and populate variable to hold the current year
+
+let year_today = date_today.getFullYear()
+
+// initialize and populate variable to hold the current time 
+let hour = date_today.getHours()
+let minutes = date_today.getMinutes()
+let seconds = date_today.getSeconds()
+
+// Dom update footer with today's date
+document.querySelector("#todaydate").innerHTML = `Last Updated ${month_today}/${day_today}/${year_today} ${hour}:${minutes}:${seconds}`;
+
+// Dom update header with today's date in full date format
+let formatted_date_today = new Date();
+const datefield = document.querySelector("date");
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(formatted_date_today);
+datefield.innerHTML = `<em>${fulldate}</em>`
+
+// Dom update footer with current copyright year
+const yearfield = document.querySelector("#yeartoday");
+yearfield.innerHTML = `Copyright ${year_today}`
+
+// Check for element in Join form.. if element label "ftime" exists process the hidden field named "ftime" else do nothing
+
+// Section for Dom update hidden form with current date  
+var ftimeexists = document.getElementById("ftime");
+if (ftimeexists) {
+// element "ftime" DOES exist so let's place todays date and time into the hidden join.html form field "ftime"
+const fulldatetime = `${month_today}/${day_today}/${year_today} ${hour}:${minutes}:${seconds}`;
+document.getElementById("ftime").value = fulldatetime;
+// Log to console to make sure we incorporated the form input time into the hidden DOM field labeled "ftime"
+console.log(document.getElementById("ftime").value)
+}
---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,47 @@
+//  function to pull in weather 
+const requestURL = "http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=efe08699ddd1c3e69ab9fb7992f15d2a";
+// const cards = document.querySelector('.cards');
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        // temporary checking for valid response and data parsing
+        console.log(jsonObject);
+        document.querySelector('#current-temp').textContent = jsonObject.main.temp;
+        const openweather = jsonObject['main'];
+        const iconsrc= `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+        const desc = jsonObject.weather[0].description;
+        document.querySelector('#icon-src').textContent = iconsrc;
+        document.querySelector('#weathericon').setAttribute('src', iconsrc);
+        document.querySelector('#weathericon').setAttribute('alt', desc);
+        document.querySelector('figcaption').textContent = desc;
+       // prophets.forEach(displayProphets);
+    });
+
+const openweather = jsonObject['main'];
+
+/*function displayProphets(prophet) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let p = document.createElement('p');
+    let porder = thrd(x);
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    p.textContent = `${prophet.name} ${prophet.lastname} was borne on ${prophet.birthdate} in the state of ${prophet.birthplace}`;
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${porder} Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2);
+    card.appendChild(portrait);
+    card.appendChild(p);
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    cards.appendChild(card);
+}*/
